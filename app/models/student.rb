d2f65ecf8185fa50948f97b8f676cca85dcba6d1
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-09 17:20:45 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,7 @@ class Student < ActiveRecord::Base
 
 
   def to_s
-    @student = [first_name, last_name].to_s * ''
+    # @student = [first_name, last_name].to_s * ''
+    @student.each do |name|
   end
 end
