640676dd74b6819a819a6aee63fed9bd6919b6bb
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-09 17:18:08 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,7 @@
 class Student < ActiveRecord::Base
   attr_accessor :first_name, :last_name, :students
 
-def initialize(student)
-  @student = student
-end
+
 
   def to_s
     @student = [first_name, last_name].to_s * ''
